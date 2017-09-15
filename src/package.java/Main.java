34b79d6702c908c85ddf1f1116a4fd194e54acc4
Revathi-Radhakrishnan <31979440+Revathi-Radhakrishnan@users.noreply.github.com>
2017-09-15 18:57:47 +0530

    Add files via upload

---
+++ src/package.java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Student s1=new Student(101,"revathi","1997-12-07",97.3);
+		Student s2=new Student(102,"sridevi",1994-05-01",96.5);
+		Student s3=new Student(103,"yuvarani",1995-12-12",95.5);
+		ArrayList<Student> al=new arrayList<Student>length();
+		al.add(s1);
+		al.add(s2);
+		al.add(s3);
+		Student[] students=al.toArray();
+	}
+
+}
